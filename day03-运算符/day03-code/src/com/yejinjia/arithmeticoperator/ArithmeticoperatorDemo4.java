ae846e593cf846ec344b59a7d3a318758b29ad5b
yjj0716 <86789608+yjj0716@users.noreply.github.com>
2024-04-11 23:59:45 +0800

    Delete day03-运算符/day03-code directory

--- day03-运算符/day03-code/src/com/yejinjia/arithmeticoperator/ArithmeticoperatorDemo4.java
+++
@@ -1,9 +0,0 @@
-package com.yejinjia.arithmeticoperator;
-
-public class ArithmeticoperatorDemo4 {
-    public static void main(String[] args) {
-        char c = 'a';
-        int result = c + 0;
-        System.out.println(result);
-    }
-}
